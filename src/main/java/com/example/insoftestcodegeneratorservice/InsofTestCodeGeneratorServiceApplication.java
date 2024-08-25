e617296feaf6743e8d8c902cfb8b5fb617b16959
Khusrav837@gmail.com <Khusrav_2011@mail.ru>
2024-08-25 21:04:26 +0500

    first commit

---
+++ src/main/java/com/example/insoftestcodegeneratorservice/InsofTestCodeGeneratorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.insoftestcodegeneratorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsofTestCodeGeneratorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsofTestCodeGeneratorServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/insoftestcodegeneratorservice/InsofTestCodeGeneratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insoftestcodegeneratorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsofTestCodeGeneratorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
